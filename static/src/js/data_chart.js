40dd472c3157b5d4b2c7fc70df2df3e8cd82bfb0
Andrii Dutchak <opora.foto@gmail.com>
2019-10-29 17:53:11 +0200

    Added some statistic data to frontend.

--- static/src/js/data_chart.js
+++ static/src/js/data_chart.js
@@ -6,6 +6,8 @@ import {getCookie, csrfSafeMethod} from "./ajax_helpers";
 export default function DataChart(obj) {
 
     let self = this;
+    let charts_positive_color = "#00ff00";
+    let charts_negative_color = "#ff0000";
 
     function requestStart() {
         obj.addClass("loading");
@@ -21,35 +23,63 @@ export default function DataChart(obj) {
 
     function renderChart(data) {
         let chart_title = obj.attr("data-title");
-        let chart_border_color = obj.attr("data-bcolor");
-        let chart_point_color = obj.attr("data-pcolor");
-        let chart_data = data.map(function(it) {
-            return it["tonality_index"];
-        });
-        let chart_labels = data.map(function(it) {
+        let chart_labels = [];
+        let data_positive = [];
+        let data_negative = [];
+        let count_val = data.length;
+        for (let i=0;i<count_val;i++) {
+            let it = data[i];
             let it_date = new Date(it["news_date"]);
-            return it_date.getMonth() + "/" + it_date.getDate() + " " + it_date.getHours() + ":" + it_date.getMinutes();
-        });
+            chart_labels.push(it_date.getMonth()+1 + "/" + it_date.getDate() + " " + it_date.getHours() + ":" + it_date.getMinutes());
+            if (it["tonality_index"] >= 0) {
+                data_positive.push(it["tonality_index"]);
+                data_negative.push(0);
+            } else {
+                data_positive.push(0);
+                data_negative.push(it["tonality_index"]);
+            }
+        }
         let ctx = obj[0].getContext("2d");
         let chart = new Chart(ctx, {
-            type: "line",
+            type: "bar",
             data: {
                 labels: chart_labels,
-                datasets: [{
-                    label: chart_title,
-                    data: chart_data,
-                    backgroundColor: "transparent",
-                    borderColor: chart_border_color,
-                    pointBackgroundColor: chart_point_color,
-                    lineTension: 0,
-                    pointBorderWidth: 0,
-                    borderWidth: 1
-                }]
+                datasets: [
+                    {
+                        label: "Positive",
+                        data: data_positive,
+                        backgroundColor: charts_positive_color,
+                        barPercentage: 1.0
+                    },
+                    {
+                        label: "Negative",
+                        data: data_negative,
+                        backgroundColor: charts_negative_color,
+                        barPercentage: 1.0
+                    }
+                ]
             },
             options: {
+                title: {
+                    display: true,
+                    text: chart_title
+                },
                 legend: {
                     display: false
-                }
+                },
+				responsive: true,
+				scales: {
+					xAxes: [{
+						stacked: true,
+					}],
+					yAxes: [{
+						stacked: true,
+                        ticks: {
+                            suggestedMin: 10,
+                            suggestedMax: 10
+                        }
+					}]
+				}
             }
         });
     }
