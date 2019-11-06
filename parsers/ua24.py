d20f48f1b36dfa49ed30f25128d7481f4a2b98bf
Andrii Dutchak <opora.foto@gmail.com>
2019-11-06 17:24:45 +0200

    Added the new parsers.

---
+++ parsers/ua24.py
@@ -0,0 +1,54 @@
+"""Module for 24 Channel site."""
+import feedparser
+from datetime import datetime
+import pytz
+from bs4 import BeautifulSoup
+from config.logger import AppLogger
+import os
+
+
+class SourceParser:
+    """Main class for 24 Channel site parser."""
+
+    src = "https://24tv.ua/rss/all.xml"
+
+    def __init__(self):
+        """Init main class."""
+        self.logger = AppLogger("parser_uapravda")
+
+    def get_news(self):
+        """Get new posts from current source."""
+        d = feedparser.parse(self.src)
+        news = []
+        for n in d.entries:
+            tz = pytz.timezone("UTC")
+            date = datetime(
+                n.published_parsed[0],
+                n.published_parsed[1],
+                n.published_parsed[2],
+                n.published_parsed[3],
+                n.published_parsed[4],
+                n.published_parsed[5],
+                0,
+                tz
+            )
+            text = ""
+            soup = BeautifulSoup(n.description, "html.parser")
+            text_div = soup.find("body")
+            if text_div:
+                for script in text_div(["script", "style"]):
+                    script.decompose()
+                text = text_div.get_text().strip()
+                text = os.linesep.join([s for s in text.splitlines() if s])
+            else:
+                logger_message = "Can not find description for {}".format(
+                    n.link
+                )
+                self.logger.write_error(logger_message)
+            news.append({
+                "title": n.title,
+                "link": n.link,
+                "date": date,
+                "text": text
+            })
+        return news
